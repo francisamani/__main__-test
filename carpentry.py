552641366281ac5367e5728a9094d9dbe872c242
Francis Amani <37621187+francisamani@users.noreply.github.com>
2018-07-05 23:49:41 +0300

    Add files via upload

---
+++ carpentry.py
@@ -0,0 +1,3 @@
+import journalism
+
+print "Least Popular Student's Club: {}".format(__name__)
---
+++ journalism.py
@@ -0,0 +1,9 @@
+def main():
+    print "Most Popular Student's Club: {}".format(__name__)
+
+# A check to see if it's the main file
+if __name__ == '__main__':
+    main()
+
+# A sign that the file isn't the main file
+else print('This is not the main page.')
